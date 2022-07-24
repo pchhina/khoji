9761a690fd5ee766c2d13e39f433e00b56cc6637
Paramjot Singh <param.chhina@gmail.com>
2022-07-24 10:19:19 -0500

    mongodb connected

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from 'next-auth/providers/github'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "./lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -7,5 +9,13 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     })
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  database: process.env.DB_URL,
+  session: {
+    jwt: true
+  },
+  jwt: {
+    secret: 'alkjkjls'
+  }
 }) 
