750e848f9c75c35d80eebcae001381dd5adc7827
Paramjot Singh <param.chhina@gmail.com>
2022-07-23 22:03:23 -0500

    github auth added

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import styles from '../styles/Nav.module.css'
+import Login from './login-btn'
 
 export default function Nav() {
     return (
@@ -8,6 +9,7 @@ export default function Nav() {
                 <li>
                     <Link href='/'>Home</Link>
                     <Link href='/sprints'>Sprints</Link>
+                    <Login />
 
                 </li>
             </ul>
