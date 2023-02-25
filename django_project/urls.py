0539bd38ab4c8862c5bb1f7645198ebf0e7ecccd
Paramjot Singh <param.chhina@gmail.com>
2023-02-25 12:16:06 -0600

    email setup to send password reset, console

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,8 +19,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("account/", include("accounts.urls")),
-    path("account/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("passions/", include("passions.urls")),
     path("", include("pages.urls")),
 ]
